7aa906d69ee98bab08c2196bead1771cfef03252
Ashley Domineck <adouglas0721@gmail.com>
2025-07-30 17:00:14 -0400

    lab submission

---
+++ main.js
@@ -0,0 +1,91 @@
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
+
+// Check two: at least two odd numbers
+// Here, we use modulus to check if something is odd.
+// Since % 2 is 0 if even and 1 if odd, we can use
+// arithmetic to count the total number of odd numbers.
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+
+// Check three: no number larger than 25
+// This time, we use the OR operator to check
+// if ANY of the numbers is larger than 25.
+//const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+//s
+// Check four: all unique numbers
+// This is long, and there are more efficient
+// ways of handling it with other data structures
+// that we will review later.
+const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+
+// Here, we put the results into a single variable 
+// for convenience. Note how we negate isOver25 using
+// the ! operator. We could also have tested for 
+// "isUnder25" as an alternative.
+//const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+
+// Finally, log the results.
+console.log(isValid);
+
+// Here's another example of how this COULD be done,
+// but it SHOULD NOT be done this way. As programmers,
+// we break things into small, manageable pieces so that
+// they can be better understood, scaled, and maintained.
+//const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
+  //((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
+  //!(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
+  //(n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+//Check if all numbers are divisible by 5. Cache the result in a variable.
+
+const isDisvisible = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0;
+console.log ("True if they are disvisble. False if they are not: "+ isDisvisible)
+// Check if the first number is larger than the last. Cache the result in a variable.
+const first_last = n1 > n4;
+console.log("True or False. Is the first number greater than last number: " + first_last);
+// Accomplish the following arithmetic chain:
+// Subtract the first number from the second number.
+const subtract_numbers = n2 - n1;
+console.log("The first number minus the second number is "+ subtract_numbers)
+// Multiply the result by the third number.
+const multi_numbers= subtract_numbers * n3;
+console.log("The result multiply by 3 is " + multi_numbers);
+// Find the remainder of dividing the result by the fourth number.
+const dividing_remainder= multi_numbers / n4;
+console.log("The result divided by 4 is " + multi_numbers);
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+const isOver25 = n1 > 25 && n2 > 25 && n3 > 25 && n4 > 25;
+
+const isValid = isSum50 && isTwoOdd && isOver25 && isUnique;
+
+// Finally, log the results.
+console.log(isValid);
+
+//--------------- Part 2
+
+// How many gallons of fuel will you need for the entire trip?
+const trip_miles= 1500;
+const budget= 175;
+const average_cost= 3;
+
+const fuelMiles_A =55; const fuelGallon_A = 30;
+//const entire_trip
+//console.log("If you are going 55 miles per hour than:");
+const fuelMiles_B = 60; const fuelGallon_B = 28;
+const fuelMiles_C = 75; const fuelGallon_C = 23;
+
+
+
+// Will your budget be enough to cover the fuel expense?
+// How long will the trip take, in hours?
+// Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
+
+const test=0;
